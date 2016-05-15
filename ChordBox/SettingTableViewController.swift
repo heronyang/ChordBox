3dd80ae97c8322b12f8c0885b7ce3fca2a0b91c1
Heron Yang <heron.yang.tw@gmail.com>
2016-05-15 22:05:17 +0800

    add credit

--- ChordBox/SettingTableViewController.swift
+++ ChordBox/SettingTableViewController.swift
@@ -10,4 +10,38 @@ import UIKit
 
 class SettingTableViewController: UITableViewController {
 	
+	/*
+	override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+	if(self.tableView.respondsToSelector(Selector("setSeparatorInset:"))){
+	self.tableView.separatorInset = UIEdgeInsetsZero
+	}
+	
+	if(self.tableView.respondsToSelector(Selector("setLayoutMargins:"))){
+	self.tableView.layoutMargins = UIEdgeInsetsZero
+	}
+	
+	if(cell.respondsToSelector(Selector("setLayoutMargins:"))){
+	cell.layoutMargins = UIEdgeInsetsZero
+	}
+	}
+	*/
+	
+	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		
+		let s = indexPath.section
+		let r = indexPath.row
+		
+		if s == 1 && r == 0 {
+			openUrl("http://www.heron.me")
+		} else if s == 1 && r == 1 {
+			openUrl("http://pason-nosap-music-factory.weebly.com/")
+		} else if s == 1 && r == 2 {
+			openUrl("https://www.linkedin.com/in/wuning0219")
+		}
+	}
+	
+	func openUrl(url: String) {
+		UIApplication.sharedApplication().openURL(NSURL(string: url)!)
+	}
+	
 }
